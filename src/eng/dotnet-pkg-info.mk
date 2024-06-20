b64fb0a6c22b6825dd5244d7f7609d5d123c011c
Dominik Viererbe <dominik.viererbe@canonical.com>
2024-06-20 04:14:53 +0300

    feat: unify redundent or similar files
    Because the debian/rules file allows for minimal scripting it was
    possible to unify all versions into one.
    
    One notable change is replacing the .in files with the deb-substvars(5)
    mechanism which allowed further unification of the control files.
    
    Note: There might be some behavior changes. This version is untested.
    
    Signed-off-by: Dominik Viererbe <dominik.viererbe@canonical.com>

---
+++ src/eng/dotnet-pkg-info.mk
@@ -0,0 +1,71 @@
+# This Makefile fragment defines the following package information variables:
+#
+#  DOTNET_MAJOR: Major .NET version number from the latest changelog entry.
+#  DOTNET_MINOR: Minor .NET version number from the latest changelog entry.
+#  DOTNET_RUNTIME_ID: .NET runtime identifier of the current host.
+#  DOTNET_ARCH: .NET architecture identifier of the current host.
+#  DOTNET_GIT_REPO: URI of the repository where the origiinal source is from.
+#  DOTNET_GIT_COMMIT: Commit hash of the original source in the repository.
+#  DOTNET_DEB_VERSION_SDK_ONLY: The version for binary deb packages that only contain SDK components in compliance with FO127.
+#  DOTNET_DEB_VERSION_RUNTIME_ONLY: The version for binary deb packages that only contain Runtime components in compliance with FO127.
+#  DOTNET_CONTAINS_BOOTSTRAPPING_SDK: A boolean (true/false) that indicates if the source packages contains an embedded SDK for initial bootstrapping.
+#  DOTNET_60_OR_GREATER: A boolean (true/false) that indicates if the source version is equal or greater than 6.0
+#  DOTNET_70_OR_GREATER: A boolean (true/false) that indicates if the source version is equal or greater than 7.0
+#  DOTNET_80_OR_GREATER: A boolean (true/false) that indicates if the source version is equal or greater than 8.0
+
+DOTNET_MAJOR = $(shell $(CURDIR)/debian/eng/dotnet-version.py --major)
+DOTNET_MINOR = $(shell $(CURDIR)/debian/eng/dotnet-version.py --minor)
+
+ifeq ($(DOTNET_MAJOR),6)
+	DOTNET_60_OR_GREATER = true
+	DOTNET_70_OR_GREATER = true
+	DOTNET_80_OR_GREATER = true
+else ifeq ($(DOTNET_MAJOR),7)
+	DOTNET_60_OR_GREATER = false
+	DOTNET_70_OR_GREATER = true
+	DOTNET_80_OR_GREATER = true
+else ifeq ($(DOTNET_MAJOR),8)
+	DOTNET_60_OR_GREATER = false
+	DOTNET_70_OR_GREATER = false
+	DOTNET_80_OR_GREATER = true
+else
+    $(error ".NET version $(DOTNET_MAJOR).$(DOTNET_MINOR) not handled")
+endif
+
+ifeq ($(DEB_HOST_ARCH), amd64)
+    DOTNET_ARCH = x64
+else ifeq ($(DEB_HOST_ARCH), arm64)
+    DOTNET_ARCH = arm64
+else ifeq ($(DEB_HOST_ARCH), s390x)
+    DOTNET_ARCH = s390x
+else ifeq ($(DEB_HOST_ARCH), ppc64el)
+    DOTNET_ARCH = ppc64le
+else
+	$(error "architecture '$(DEB_HOST_ARCH)' not handled")
+endif
+
+DOTNET_RUNTIME_ID = $(shell source /etc/os-release; echo $${ID}.$${VERSION_ID}-$(DOTNET_ARCH))
+
+ifeq ($(DOTNET_80_OR_GREATER), true)
+    DOTNET_DEB_VERSION_SDK_ONLY = $(shell $(CURDIR)/debian/eng/dotnet-version.py --sdk-only-deb-version)
+    DOTNET_DEB_VERSION_RUNTIME_ONLY = $(shell $(CURDIR)/debian/eng/dotnet-version.py --runtime-only-deb-version)
+    
+    ifeq ("$(wildcard $(CURDIR)/release.info)",)
+        $(error "No release.info file in the source tarball found")
+    endif
+
+    DOTNET_GIT_REPO = $(shell grep --only-matching --perl-regexp '(?<=^RM_GIT_REPO=).+$$' $(CURDIR)/release.info)
+    DOTNET_GIT_COMMIT = $(shell grep --only-matching --perl-regexp '(?<=^RM_GIT_COMMIT=).+$$' $(CURDIR)/release.info)
+
+	ifeq ($(DOTNET_GIT_REPO),)
+		$(error ".NET git source repository is NOT specified in release.info file.")
+    else ifeq ($(DOTNET_GIT_COMMIT),)
+        $(error ".NET git commit hash is NOT specified in release.info file.")
+	endif
+endif
+
+ifneq ("$(wildcard $(CURDIR)/.dotnet)",)
+    DOTNET_CONTAINS_BOOTSTRAPPING_SDK = true
+else
+    DOTNET_CONTAINS_BOOTSTRAPPING_SDK = false
+endif
--- src/eng/dotnet-version.py
+++ src/eng/dotnet-version.py
@@ -12,4 +12,34 @@ if __name__ == "__main__":
     version = SourcePackageVersion.ParseFromChangelog(
         os.path.join("debian", "changelog"))
     
-    print(f"{version.SdkVersion.Major}.{version.SdkVersion.Minor}")
+    if (len(sys.argv) <= 1):
+        print(f"{version.SdkVersion.Major}.{version.SdkVersion.Minor}")
+    elif (sys.argv[1] == "--major"):
+        print(f"{version.SdkVersion.Major}")
+    elif (sys.argv[1] == "--minor"):
+        print(f"{version.SdkVersion.Minor}")
+    elif (sys.argv[1] == "--sdk"):
+        print(str(version.SdkVersion))
+    elif (sys.argv[1] == "--runtime"):
+        print(str(version.RuntimeVersion))
+    elif (sys.argv[1] == "--runtime-only-deb-version"):
+        versionString = str(version.RuntimeVersion)        
+
+        if (version.BootstrapArchitecture != None):
+            versionString += f"~bootstrap+{version.BootstrapArchitecture}"
+
+        versionString += f"-{version.DebRevision}"
+
+        print(versionString)
+    elif (sys.argv[1] == "--sdk-only-deb-version"):
+        versionString = str(version.SdkVersion)        
+
+        if (version.BootstrapArchitecture != None):
+            versionString += f"~bootstrap+{version.BootstrapArchitecture}"
+
+        versionString += f"-{version.DebRevision}"
+
+        print(versionString)
+    else:
+        print("invalid arguments")
+        sys.exit(1)
