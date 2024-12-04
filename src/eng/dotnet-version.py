canonical/dotnet-source-build

commit f89798a1382d7ac19a32ded63989caabe7723764

chore: partialy revert tmp version ordering fix
This partially reverts commit ea194a15a14ee63bfc79ac9e1ffa88cfbe988015.

It removes the temporarily added '-rtm' to DOTNET_DEB_VERSION_SDK_ONLY,
because the .NET SDK now has an incremented patch number '9.0.101' (new)
vs '9.0.100' (old). The runtime fix has to persist, because the runtime
version number did not change with the December 2024 release.

src/eng/dotnet-version.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
         if (version.BootstrapArchitecture != None):
             versionString += f"~bootstrap+{version.BootstrapArchitecture}"
 
-        versionString += f"-rtm-{version.DebRevision}"
+        versionString += f"{version.DebRevision}"
 
         print(versionString)
     else:

commit 8f312d5145541d14cdaec40d3f9263d4684e3fca

fix: sdk components version format error
I discovered that I introduced a bug with commit
f89798a1382d7ac19a32ded63989caabe7723764

I wanted to remove `-rtm`, but accidentially removed `-rtm-` (one `-`
too much). This would result in incorrect version numbers and therefore
version ordering when fixed.

Example:
A test build of the previous commit resulted in a dotnet-sdk-9.0 package
with the version number `9.0.1010ubuntu1`.
Correct would be `9.0.101-0ubuntu1`.

src/eng/dotnet-version.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
         if (version.BootstrapArchitecture != None):
             versionString += f"~bootstrap+{version.BootstrapArchitecture}"
 
-        versionString += f"{version.DebRevision}"
+        versionString += f"-{version.DebRevision}"
 
         print(versionString)
     else: