d5ed45327c44bbe8d1e8c5f7eb0f773360741f25
Mateus Rodrigues de Morais <mateus.morais@canonical.com>
2025-12-09 20:32:44 -0300

    chore: reference src/tests/regular-tests with flamenco.json

--- src/tests/regular-tests/publish-ready-to-run/test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-IFS=$'\n\t'
-set -x
-
-dotnet new console --no-restore
-dotnet publish -r "$(../runtime-id --sdk)" /p:PublishReadyToRun=true
--- src/tests/regular-tests/restore-with-rid/test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-IFS=$'\n\t'
-set -x
-
-dotnet new console --no-restore
-dotnet restore -r "$(../runtime-id --sdk)"
